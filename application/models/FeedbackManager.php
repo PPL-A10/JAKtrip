f7b52a47663dbc1df417a088b4144ed469940b11
Syifa Khairunnisa <syifa.kha@gmail.com>
2015-03-31 19:57:33 +0700

    cobain feedback cantik

---
+++ application/models/FeedbackManager.php
@@ -0,0 +1,14 @@
+<?php
+
+class FeedbackManager extends CI_Model{
+    function __construct() {
+        parent::__construct();
+    }
+    
+ function insert_feedback($data){
+ //Inserting in Table(feedback) of Database(jaktrip) 
+       $this->db->insert('feedback', $data);
+    } 
+}
+
+?>
