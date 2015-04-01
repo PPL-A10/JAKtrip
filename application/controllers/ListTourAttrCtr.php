eb2eea2f48f91490907eabe66a7823d0aa60c706
Syifa Khairunnisa <syifa.kha@gmail.com>
2015-04-01 15:51:30 +0700

    commit aja

---
+++ application/controllers/ListTourAttrCtr.php
@@ -0,0 +1,17 @@
+<?php
+
+class TourAttrCtr extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('TouristAttractionManager');
+       
+    }
+
+    function index()
+	{   
+		
+	}
+}
+
+?>
---
+++ application/controllers/RatingCtr.php
@@ -0,0 +1,55 @@
+<?php
+
+class FeedbackCtr extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('ratingManager');
+       
+    }
+
+    function index()
+	{   
+		//Including validation library
+		$this->load->library('form_validation');
+                
+        $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		
+		//kayanya ini set_rules('id dari view', 'nama coloumn di db', 'keterangan tambahan')
+		
+		//Validating Name Field
+		$this->form_validation->set_rules('name', 'name', 'required');
+		
+		//Validating Email Field
+		$this->form_validation->set_rules('email', 'email', 'required|valid_email');
+		
+		//Validating Subject Field
+		$this->form_validation->set_rules('subject', 'subject');
+		
+		//Validating Address Field
+		$this->form_validation->set_rules('message', 'message', 'required');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('formRatingUI');
+		}
+		else
+		{
+			//Setting values for tabel columns
+			$data = array(
+                        'name' => $this->input->post('name'),
+                        'email' => $this->input->post('email'),
+                        'subject' => $this->input->post('subject'),
+                        'message' => $this->input->post('message')
+                    );
+			
+
+					//Transfering data to Model
+                   $this->feedbackManager->insert_feedback($data);
+                    //Loading View
+					$this->load->view('formRatingUI');
+          }
+	}
+}
+
+?>
