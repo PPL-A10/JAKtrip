10a45ec37cb473f431927eeda6ffd07ce635998d
mohammadsyahidw <mohammad.syahid.wildan@gmail.com>
2015-04-03 18:00:08 +0700

    asa

---
+++ application/views/homeUI.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  </head>
+  <title>JAKtrip</title>
+  <body>
+	<form id="your_form" action="listTourAttrCtr" method="post">
+		<input type="submit" name="tombolGetAllTourAttr" value="lihat semua tempat wisata" />
+	</form>
+	
+  </body>
+</html>
