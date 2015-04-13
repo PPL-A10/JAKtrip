f8e80d00dab63760494cd076ff98e6d867528c01
fakhirahdg <fakhirah.dg@gmail.com>
2015-04-13 15:21:34 +0700

    coba

--- application/views/formTourAttrUI.php
+++ application/views/formTourAttrUI.php
@@ -1,5 +1,4 @@
-<html>
-	<head>
+<head>
 	<script
 src="http://maps.googleapis.com/maps/api/js">
 </script>
@@ -54,141 +53,200 @@ function addLocation(lng,lat){
 }
 </script>
 	</head>
-	<body>
-	
-		<div id="container">
-			<div id="header">
+
+
+			<div class="col-lg-12">
+				<!-- if succes
+				<div class="col-lg-2"></div>
+				<div class="alert alert-dismissible alert-success col-lg-7" style="text-align: center; margin: 15px;">
+				<button type="button" class="close" data-dismiss="alert">×</button>
+				<strong>Thank you!</strong> You successfully published new places. </div>
+				<div class="col-lg-3"></div><br><br><br><br>
 				
-			</div>
-			<div id="menu">
-				
-			</div>
-			<div id="content">
-			
-			<h1>Add New Post</h1>
+				if failed
+				<div class="col-lg-2"></div>
+				<div class="alert alert-dismissible alert-warning col-lg-7" style="text-align: center; margin: 15px;">
+				<button type="button" class="close" data-dismiss="alert">×</button>
+				<strong>Ooops!</strong> Something went wrong. Please double check the form. </div>
+				<div class="col-lg-3"></div><br><br><br><br>
+			-->
+
+				<div class="tuffyh2a admintitle">Add New Post</div>
+
 				<?php
-				$attributes = array('class' => '', 'id' => '');
-				echo form_open_multipart('tourAttrCtr/myform', $attributes); ?>
-	
-				<p>
-						<label for="place_name">Name <span class="required">*</span></label>
+				$attributes = array('class' => 'newpost col-lg-8', 'method' => 'post');
+				echo form_open('tourAttrCtr/myform', $attributes); ?>
+		
+					<div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Title <span class="req">*</span></label>
 						<?php echo form_error('place_name'); ?>
-						<br /><input id="place_name" type="text" name="place_name"  value="<?php echo set_value('place_info'); ?>"  />
-				</p>
-
-				<p>
-						<label for="description">Description <span class="required">*</span></label>
+  						<input class="form-control" type="text" id="place_name" name="place_name" value="<?php echo set_value('place_name'); ?>" >
+				      <br></div>
+				    </div>
+					<br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Description <span class="req">*</span></label>
 						<?php echo form_error('description'); ?>
-						<br />
-											
-						<?php echo form_textarea( array( 'name' => 'description', 'rows' => '5', 'cols' => '80', 'value' => set_value('description') ) )?>
-				</p>
-				<!--
-				<p>
-						<label for="place_info">Place Info <span class=""></span></label>
-						<?php //echo form_error('place_info'); ?>
-						<br /><input id="place_info" type="text" name="place_info"  value="<?php //echo set_value('place_info'); ?>"  />
-				</p>
-				-->
-				
-				<?php echo "Place Info ", form_dropdown('place_info',$place_info, set_value('place_info')); ?>
-				
-				<p>
-						<label for="weekday_price">Weekday Price <span class="required">*</span></label>
+  						<textarea class="form-control" rows="3" id="textArea" id="description" name="description" value="<?php echo set_value('description'); ?>" ></textarea>
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Place Information</label><br>
+						<?php echo form_error('place_info'); ?>
+   					 	<span class="field custom-dropdown ">
+   					 	<select   class="field form-control" title="Place Information" style="margin-left: -10px;">    
+   					     	<option value="" selected disabled>Choose place information..</option>
+   					     	<?php
+			                  foreach($place as $row)
+			                  {
+			                    echo "<option value='".$row->place_name."'>".$row->place_name."</option>";
+			                  }
+			                ?>
+   					 	</select>
+   					 </span><br>
+				      <br></div>
+				    </div>
+				    <br><br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Weekday Price <span class="req">*</span></label>
 						<?php echo form_error('weekday_price'); ?>
-						<br /><input id="weekday_price" type="text" name="weekday_price"  value="<?php echo set_value('weekday_price'); ?>"  />
-				</p>
-
-				<p>
-						<label for="weekend_price">Weekend Price <span class="required">*</span></label>
+  						<input class="form-control" type="text" id="weekday_price" name="weekday_price" value="<?php echo set_value('weekday_price'); ?>" >
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Weekend Price <span class="req">*</span></label>
 						<?php echo form_error('weekend_price'); ?>
-						<br /><input id="weekend_price" type="text" name="weekend_price"  value="<?php echo set_value('weekend_price'); ?>"  />
-				</p>
-				
-				
-				<?php //echo "Category ", form_input('category_name',set_value('category_name')); ?>
-				
-				<?php //foreach($cat_name as $row){
-						//echo "<input type='checkbox' name='category_list[]' value=".$row->category_name.">".$row->category_name."<br>";
-				//}
-				//echo "<label><input type='checkbox' name='category_list[]' value='new_cat'>New Category ".form_input('category_name',set_value('category_name'))."</label><br>";
-				?>
-				
-				<?php foreach ($cat_name as $row){
-					echo form_checkbox('category_list[]',$row->category_name).($row->category_name)."<br>"; 
-				}
-				echo form_checkbox('category_list[]','')."Other ".form_input('category_new',set_value('category_new'))."<br>";
-				?>
-			
-				
-				<?php $loc = array('Jakarta Barat' => 'Jakarta Barat', 'Jakarta Pusat' => 'Jakarta Pusat', 'Jakarta Selatan' =>'Jakarta Selatan', 
-					'Jakarta Timur' => 'Jakarta Timur', 'Jakarta Utara' =>'Jakarta Utara'); 
-				echo "Location ", form_dropdown('city',$loc, set_value('city')); ?>
-				
-				<p>
-						<label for="pic">Photos <span class=""></span></label>
-						<?php echo form_error('pic'); 
-
+  						<input class="form-control" type="text" id="weekend_price" name="weekend_price" value="<?php echo set_value('weekend_price'); ?>" >
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Category <span class="req">*</span></label><br>
+						<?php echo form_error('category_name'); ?>
+  						<?php foreach ($cat_name as $row){
+							echo form_checkbox('category_list[]',$row->category_name).($row->category_name)."<br>"; 
+						}
+						echo '<br>';
+						echo form_checkbox('category_list[]','')."Other ".form_input('category_new',set_value('category_new'))."<br>";
 						?>
-						<br /><input id="pic" type="file" name="pic[]" size="20"  value="<?php echo set_value('pic'); ?>" multiple>
-				</p>
-				
-				<p>
-						<label for="pic_info">Photos Info <span class=""></span></label>
-						<?php echo form_error('pic_info'); ?>
-						<br /><input id="pic_info" type="text" name="pic_info"  value="<?php echo set_value('pic_info'); ?>"  />
-				</p>
-				
-				<p>
-						<label for="longitude">Longitude <span class="required">*</span></label>
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Location <span class="req">*</span></label><br>
+						<?php echo form_error('city'); ?>
+  						<span class="field custom-dropdown ">
+   					 	<select   class="field form-control" title="All Location" style="margin-left: -10px;">    
+   					     	<option value="" selected disabled>All Location</option>
+   					     	<?php
+			                  foreach($query2 as $row)
+			                  {
+			                    echo "<option value='".$row->city."'>".$row->city."</option>";
+			                  }
+			                ?>
+   					 	</select>
+   					 </span><br>
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Photos <span class="req">*</span></label>
+						<?php echo form_error('city'); ?>
+  						<input class="" type="file" id="pic" name="pic" value="<?php echo set_value('pic'); ?>" multiple>
+				      <br></div>
+				    </div>
+				    <br>
+					<div class="form-group">
+					  <div class="col-lg-11">
+						<div id="googleMap" style="width:500px;height:380px;"></div>
+				      <br></div>
+				    </div>
+				    <br>
+					
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Longitude <span class="req">*</span></label>
 						<?php echo form_error('longitude'); ?>
-						<br /><input id="longitude" type="text" name="longitude"  value="<?php echo set_value('longitude'); ?>"  />
-				</p>
-				
-				<p>
-						<label for="lattitude">Lattitude <span class="required">*</span></label>
+  						<input class="form-control" type="text" id="longitude" name="longitude" value="<?php echo set_value('longitude'); ?>" >
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Lattitude <span class="req">*</span></label>
 						<?php echo form_error('lattitude'); ?>
-						<br /><input id="lattitude" type="text" name="lattitude"  value="<?php echo set_value('lattitude'); ?>"  />
-				</p>
-				<div id="googleMap" style="width:500px;height:380px;"></div>
-				
-
-				<?php echo "Halte ", form_dropdown('halte_code',$halte_name, set_value('halte_code')); ?>
-				
-				<p>
-						<label for="transport_info">Transport Info <span class="required">*</span></label>
+  						<input class="form-control" type="text" id="lattitude" name="lattitude" value="<?php echo set_value('lattitude'); ?>" >
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Nearest Bus Stop <span class="req">*</span></label><br>
+						<?php echo form_error('halte_code'); ?>
+  						 <span class="field custom-dropdown " >
+   					 	<select class="field form-control" title="Nearest bus stop?" >    
+   							 <option value="" selected disabled>Nearest bus stop?</option>
+   					      	<?php
+   				    		foreach ($query as $row) {
+									echo "<option value=\"".$row->halte_name."\">".$row->halte_name."</option>";
+							}  				    		 
+   				   			?>
+   					 	</select>
+   					 </span>
+				      <br><br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Transportation Info <span class="req">*</span></label>
 						<?php echo form_error('transport_info'); ?>
-						<br /><input id="transport_info" type="text" name="transport_info"  value="<?php echo set_value('transport_info'); ?>"  />
-				</p>
-				
-				<p>
-						<label for="transport_price">Transport Price <span class="required">*</span></label>
+  						<input class="form-control" type="text" id="transport_info" name="transport_info" value="<?php echo set_value('transport_info'); ?>" >
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Transportation Fee <span class="req">*</span></label>
 						<?php echo form_error('transport_price'); ?>
-						<br /><input id="transport_price" type="text" name="transport_price"  value="<?php echo set_value('transport_price'); ?>"  />
-				</p>
-				
+  						<input class="form-control" type="text" id="transport_price" name="transport_price" value="<?php echo set_value('transport_price'); ?>" >
+				      <br></div>
+				    </div>
+				    <br>
+				    <div class="form-group">
+					  <div class="col-lg-11">
+						<label class="control-label">Author <span class="req">*</span></label>
+						 <span class="field custom-dropdown " >
+   					 	<select class="field form-control" title="Nearest bus stop?" >    
+   							 <option value="" selected disabled>Nearest bus stop?</option>
+   					      	<?php
+   				    		foreach ($admin as $row) {
+									echo "<option value=\"".$row->username."\">".$row->username."</option>";
+							}  				    		 
+   				   			?>
 
-				<?php $author = array('ahmadibrahim' => 'ahmadibrahim', 'fakhirahdg' => 'fakhirahdg', 'khusnanadia' => 'khusnanadia', 
-					'syifakha' => 'syifakha', 'mswildan' => 'mswildan'); 
-				echo "Author ", form_dropdown('author',$author, set_value('author')); ?>
-				
-				<p>
-						<?php echo form_submit( 'publish', 'Publish'); ?>
-				</p>
-				
-				<p>
-						<?php //echo form_submit( 'save', 'Save'); ?>
-				</p>
-				
-				
-				
-				<?php echo form_close();?>
+   					 	</select>
+   					 </span>
+				      <br></div>
+				    </div>
+				    <br>
 
-				<br><br><br>
+				    <button class="btn btn-warning" type="submit">PUBLISH</button>
+				    <button class="btn btn-primary" type="submit">SAVE</button>
+				    
+				<?php echo form_close(); ?>
 			</div>
-		</div>
-		<div id="footer">
-		</div>
 
-	</body>
-</html>
+
+			
+		</div>
+	</div>
