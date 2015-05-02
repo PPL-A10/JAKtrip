6102033c4d4d4cee1776959fb1744cb2cc2a6f49
Ahmad Ibrahim <amad.ibra@gmail.com>
2015-05-02 10:29:40 +0700

    Final report spam

--- application/models/SpamManager.php
+++ application/models/SpamManager.php
@@ -17,29 +17,44 @@
 		}
 		
 		function updatespam($id, $spamreason){
-		if($spamreason == 'spam'){			
-		$data = array(
+		if($spamreason == 'spam'){	
+		$this->db->where('id_rate', $id);
+		$this->db->set('is_spam', 'is_spam+1', FALSE);
+		$this->db->update('rating'); 
+		/*$data = array(
                'is_spam' => 1
-            );}
-		if($spamreason == 'false_statement'){			
-		$data = array(
+            );*/}
+		if($spamreason == 'false_statement'){
+		$this->db->where('id_rate', $id);
+		$this->db->set('is_falsestatement', 'is_falsestatement+1', FALSE);
+		$this->db->update('rating'); 
+		/*$data = array(
                'is_falsestatement' => 1
-            );}
-		if($spamreason == 'unrelated_content'){			
-		$data = array(
+            );*/}
+		if($spamreason == 'unrelated_content'){
+		$this->db->where('id_rate', $id);
+		$this->db->set('is_unrelatedcontent', 'is_unrelatedcontent+1', FALSE);
+		$this->db->update('rating'); 
+		/*$data = array(
                'is_unrelatedcontent' => 1
-            );}
-		if($spamreason == 'profanity'){			
-		$data = array(
+            );*/}
+		if($spamreason == 'profanity'){	
+		$this->db->where('id_rate', $id);
+		$this->db->set('is_profanity', 'is_profanity+1', FALSE);
+		$this->db->update('rating'); 
+		/*$data = array(
                'is_profanity' => 1
-            );}
-		if($spamreason == 'nudity'){			
-		$data = array(
+            );*/}
+		if($spamreason == 'nudity'){
+		$this->db->where('id_rate', $id);
+		$this->db->set('is_nudity', 'is_nudity+1', FALSE);
+		$this->db->update('rating'); 
+		/*$data = array(
                'is_nudity' => 1
-            );}
+            );*/}
 
-			$this->db->where('id_rate', $id);
-			$this->db->update('rating', $data); 
+			//$this->db->where('id_rate', $id);
+			//$this->db->update('rating', $data); 
 			// Produces:
 			// UPDATE mytable 
 			// SET title = '{$title}', name = '{$name}', date = '{$date}'
