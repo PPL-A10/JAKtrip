e1ba710a9826adbcba4c739701c15ba82000ed7f
mohammadsyahidw <mohammad.syahid.wildan@gmail.com>
2015-05-11 10:50:12 +0700

    benerin crud

--- application/views/profile.php
+++ application/views/profile.php
@@ -17,6 +17,8 @@ echo "<p>First Name : ".$user_profile['first_name']."</p>";
 echo "<p>Last Name : ".$user_profile['last_name']."</p>";
 echo "<p>Gender : ".$user_profile['gender']."</p>";
 echo "<p>Facebook URL : "."<a href=".$user_profile['link']." target='_blank'"."> https://www.facebook.com/".$user_profile['id']."</a></p>";
+echo "<p>Facebook Email : ".$user_profile['email']."</p>";
+echo "<p>Facebook username : ".$user_profile['username']."</p>";
 ?>
 </div>
 </div>
