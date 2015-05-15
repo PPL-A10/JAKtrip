a4873f6cb88475de3f7cf5fd31caac8ea98eb1c3
Khusna Nadia <khusnanadia@gmail.com>
2015-05-15 22:31:29 +0700

    nambahin promo sama benerin validasi promo

--- application/views/FormPromoUI2.php
+++ application/views/FormPromoUI2.php
@@ -26,7 +26,7 @@ Menampilkan form isian mengubah promo di halaman admin
 		  <div class="col-lg-11">
 			<label class="control-label">Title <span class="req">*</span></label>
 			<?php echo form_error('title'); ?>
-  				<input class="form-control" type="text" id="title" name="title" value="<?php echo $title; ?>" required pattern="[a-zA-Z0-9\s]+" title="Title can't contain any of the following characters : ! @ # $ % ^ & * &quot; &apos; ">
+  				<input class="form-control" type="text" id="title" name="title" value="<?php echo $title; ?>" required pattern="[a-zA-Z0-9%.-\s]+" title="Title can't contain any of the following characters : ! @ # $ ^ & * &quot; &apos; ">
 		      <br></div>
 		    </div>
 			<br>
